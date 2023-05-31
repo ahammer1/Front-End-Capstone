03b87c34c5c436935541f70ba7fde41447b518d4
ahamm <ahammer753@gmail.com>
2023-05-30 19:39:10 -0500

    unable to render characters

--- pages/Game/[firebaseKey].js
+++ pages/Game/[firebaseKey].js
@@ -1,16 +1,22 @@
 // this page will route to characters associated with this game
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+import Card from 'react-bootstrap/Card';
 import { Button } from 'react-bootstrap';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { getSingleGame } from '../../api/gameData';
-import { getSingleCharacter } from '../../api/CharacterData';
+// import CharacterCard from '../../components/CharacterCard';
+// import { getCharacters } from '../../api/CharacterData';
+// import { useAuth } from '../../utils/context/authContext';
+import { getCharacterGames } from '../../api/CharacterData';
+import CharacterCard from '../../components/CharacterCard';
+// import viewCharacterDetails from '../../api/mergedData';
+// import { getSingleCharacter } from '../../api/CharacterData';
 // import CharacterCard from '../../components/CharacterCard';
 
 export default function ViewGame() {
   const [gameDetails, setGameDetails] = useState({});
-  const [characterDetails, setCharacterDetails] = useState({});
+  const [characters, setCharacter] = useState({});
   const router = useRouter();
 
   const { firebaseKey } = router.query;
@@ -20,8 +26,10 @@ export default function ViewGame() {
   }, [firebaseKey]);
 
   useEffect(() => {
-    getSingleCharacter(firebaseKey).then(setCharacterDetails);
+    getCharacterGames(firebaseKey).then(setCharacter);
   }, [firebaseKey]);
+
+  console.warn({ gameDetails });
 
   return (
     <>
@@ -30,21 +38,56 @@ export default function ViewGame() {
           <Button>Add A Character</Button>
         </Link>
         <div className="text-white ms-5 details">
-          <h className="card-title bold">{gameDetails.title}</h>
-          <Image img src={gameDetails.img} alt={gameDetails.title} style={{ width: '300px' }} />
+          <h2 className="card-title bold">{gameDetails.title}</h2>
+          <Card.Img src={gameDetails.image} alt={gameDetails.title} style={{ width: '300px' }} />
           <p className="card-text bold">{gameDetails.genre}</p>
           <p className="card-text bold">{gameDetails.console}</p>
           <p className="card-text bold">{gameDetails.description}</p>
         </div>
       </div>
-      <div className="mt-5 d-flex flex-wrap">
+      {/* <div className="mt-5 d-flex flex-wrap">
         <div className="text-white ms-5 details">
-          <Image img src={characterDetails.image} alt={characterDetails.title} style={{ width: '300px' }} />
+          <Card.Img src={characterDetails.image} alt={characterDetails.title} style={{ width: '300px' }} />
           <h5>
             {characterDetails.character_name}
           </h5>
         </div>
+      </div> */}
+      <div className="d-flex flex-wrap">
+        {characters.map((character) => (
+          <CharacterCard key={character.firebaseKey} characterObj={character} />
+        ))}
       </div>
     </>
   );
 }
+
+// function CharacterHome() {
+//   const [characters, setCharacters] = useState([]);
+
+//   const { user } = useAuth ();
+
+//   const getAllCharacters = () => {
+//     getCharacters(user.uid).then(setCharacters);
+//   };
+
+//   useEffect(() => {
+//     getAllCharacters();
+//   }, []);
+
+//   return (
+//     <div className="text-center my-4">
+//       <Link href="/character/new" passHref>
+//         <Button>Add A Character</Button>
+//       </Link>
+//       <div className="d-flex flex-wrap">
+//         {characters.map((character) => (
+//           <CharacterCard key={character.firebaseKey} characterObj={character} onUpdate={getAllCharacters} />
+//         ))}
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// export default CharacterHome;
