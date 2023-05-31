7f71ea3600ea8670361f03d0d27ee9908ebfba5e
ahamm <ahammer753@gmail.com>
2023-05-30 19:42:16 -0500

    characters

--- pages/character.js
+++ pages/character.js
@@ -1,36 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { Button } from 'react-bootstrap';
-import { getCharacters } from '../api/CharacterData';
-import { useAuth } from '../utils/context/authContext';
-import CharacterCard from '../components/CharacterCard';
+// import React, { useEffect, useState } from 'react';
+// import Link from 'next/link';
+// import { Button } from 'react-bootstrap';
+// import { getCharacters } from '../api/CharacterData';
+// import { useAuth } from '../utils/context/authContext';
+// import CharacterCard from '../components/CharacterCard';
 
-function CharacterHome() {
-  const [characters, setCharacters] = useState([]);
+// function CharacterHome() {
+//   const [characters, setCharacters] = useState([]);
 
-  const { user } = useAuth();
+//   const { user } = useAuth();
 
-  const getAllCharacters = () => {
-    getCharacters(user.uid).then(setCharacters);
-  };
+//   const getAllCharacters = () => {
+//     getCharacters(user.uid).then(setCharacters);
+//   };
 
-  useEffect(() => {
-    getAllCharacters();
-  }, []);
+//   useEffect(() => {
+//     getAllCharacters();
+//   }, []);
 
-  return (
-    <div className="text-center my-4">
-      <Link href="/character/new" passHref>
-        <Button>Add A Character</Button>
-      </Link>
-      <div className="d-flex flex-wrap">
-        {characters.map((character) => (
-          <CharacterCard key={character.firebaseKey} characterObj={character} onUpdate={getAllCharacters} />
-        ))}
-      </div>
+//   return (
+//     <div className="text-center my-4">
+//       <Link href="/character/new" passHref>
+//         <Button>Add A Character</Button>
+//       </Link>
+//       <div className="d-flex flex-wrap">
+//         {characters.map((character) => (
+//           <CharacterCard key={character.firebaseKey} characterObj={character} onUpdate={getAllCharacters} />
+//         ))}
+//       </div>
 
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-export default CharacterHome;
+// export default CharacterHome;
